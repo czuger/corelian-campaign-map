a70d2bc895244e9873ae4d2ffd9a6f1c28a210e6
ced <zuger.cedric@gmail.com>
2021-04-06 06:57:11 +0200

    Can switch token type.

--- html/js/edit.js
+++ html/js/edit.js
@@ -5,14 +5,37 @@
 Vue.component('base-img', {
     data: function () {
         return {
-            count: 0
+            pic_path: 'pics/ImperialBase_sticker.png',
+            pic_status: 1
         }
     },
     props: ['pos'],
-    template: '<img class="base-class" src="pics/ImperialBase_sticker.png" :style="pos">'
+    template: '<img class="base-class" :src="pic_path" :style="pos" v-on:click="change_status">',
+    methods: {
+        change_status: function (event) {
+            switch (this.pic_status) {
+                case 1:
+                    this.pic_path =  'pics/RebelPresence_sticker.png';
+                    this.pic_status =  2;
+
+                    break;
+                case 2:
+                    this.pic_path =  'pics/RebelPresence_sticker.png';
+                    this.pic_status =  3;
+
+                    break;
+
+                case 3:
+                    this.pic_path =  'pics/PresenceDestroyed_sticker.png';
+
+                    break;
+
+                default:
+                    console.log(`Unknown pic status ${this.pic_status}`);
+            }
+        }
+    }
 })
-
-
 
 // L'objet est ajouté à une instance de Vue
 var vm = new Vue({
@@ -20,14 +43,14 @@ var vm = new Vue({
     data: { tokens: [] },
     methods: {
         onclick: function (event) {
-            console.log(event);
-            console.log(this.tokens);
+            // console.log(event);
+            // console.log(this.tokens);
             // console.log(event.clientX, event.clientY);
             var x = event.offsetX;
             var y = event.offsetY;
             var style = 'top:' + (y - 21) + 'px;left:' + (x - 21) + 'px';
-            console.log(style);
-            this.tokens.push({pos: style })
+            // console.log(style);
+            this.tokens.push({pos: style})
         }
     }
 })
