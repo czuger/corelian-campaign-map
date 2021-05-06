72f391c17ca31f118b2658983ac660530e0d4342
Cédric ZUGER <cedric.zuger@rdsdiag.com>
2021-05-06 09:54:15 +0200

    Adding the create user migration.

---
+++ db/migrate/20210505193846_create_user.rb
@@ -0,0 +1,20 @@
+class CreateUser < ActiveRecord::Migration[6.1]
+  def change
+    create_table :users do |t|
+      t.string :name, null: false
+      t.string :uid, index: { unique: true }, null: false
+      t.string :avatar, null: false
+
+      t.timestamps
+    end
+
+    create_table :players do |t|
+      t.references :user, null: false, foreign_key: true
+      t.references :campaign, null: false, foreign_key: true
+      t.string :side, null: false
+
+      t.timestamps
+    end
+
+  end
+end
