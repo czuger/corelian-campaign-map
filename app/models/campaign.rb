b1d30ab11f0fac1cf475dd3ed341bdc5f3797928
Cédric ZUGER <cedric.zuger@rdsdiag.com>
2021-11-23 22:46:57 +0100

    Fixing map to token alignment.

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -2,7 +2,7 @@ require_relative 'token'
 
 class Campaign < ActiveRecord::Base
 
-  has_many :tokens, inverse_of: :campaign, dependent: :destroy
-  has_many :players, inverse_of: :campaign, dependent: :destroy
+  has_many :tokens, dependent: :destroy
+  has_many :players, dependent: :destroy
 
 end
